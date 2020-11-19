38d6ddf991658073fea94054d71e0b64fe80ef3d
Tiago Fenix Barros <tiagofenixbarros@gmail.com>
2020-11-19 18:41:50 -0300

    Add project files.

---
+++ ConsoleAppSQLite/Program.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using System;
+
+namespace ConsoleAppSQLite
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string databasePath = @"C:\Projects\Lab\SQLite\cipher.db";
+
+            string[] maleNames = new string[10] { "aaron", "abdul", "abe", "abel", "abraham", "adam", "adan", "adolfo", "adolph", "adrian"};
+            string[] lastNames = new string[5] { "abbott", "acosta", "adams", "adkins", "aguilar"};
+            Random r = new Random();
+
+            var options = new SQLiteConnectionString(databasePath, true, key: "Nokia1234");
+            var connection = new SQLiteConnection(options);
+            connection.Execute($"INSERT INTO Users(FName, LName) VALUES('{maleNames[r.Next(0, 9)]}','{lastNames[r.Next(0, 4)]}')");
+
+            var users = connection.Query<User>("SELECT * FROM Users");
+
+            foreach(User user in users)
+            { 
+                Console.WriteLine($"{user.ID} , FIRST NAME: {user.FName} LAST NAME: {user.LName}");
+            }
+
+            connection.Close();
+
+            Console.ReadKey();
+        }
+
+        public class User
+        {
+            public int ID { get; set; }
+            public string FName { get; set; }
+            public string LName { get; set; }
+        }
+    }
+}
